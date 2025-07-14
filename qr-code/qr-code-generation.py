f613fc62a690991d00572cd1171aa86c807e13b0
Daniel W Liu <117604131+LTKers@users.noreply.github.com>
2025-07-14 10:11:59 -0400

    refactor(qr): fix naming of qr-code-generation file

--- qr-code/qr-code-generation.py
+++ qr-code/qr-code-generation.py
@@ -2,11 +2,11 @@ import qrcode
 import cv2 as cv
 from qrcode.image.pure import PyPNGImage
 
-
+#QRCode creation class
 qr=qrcode.QRCode(version=1, error_correction=qrcode.ERROR_CORRECT_H, box_size=10, border=4)
 
+#Creates QRCode image using input and saves into images directory
 block_name=input("Enter block name")
 qr.add_data(block_name)
-
 img = qr.make_image(image_factory=qrcode.image.pure.PyPNGImage, fill_color="black", back_color="white")
 imgsave=img.save(f'QR_Images/{block_name}.png')
