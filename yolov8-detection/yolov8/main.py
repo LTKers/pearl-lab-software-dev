0460f86e9a6018b6042ca6bbee7dcc6e3bf81944
Daniel W Liu <117604131+LTKers@users.noreply.github.com>
2025-07-24 14:36:26 -0400

    feat(model): add testing functionality for model

--- yolov8-detection/yolov8/main.py
+++ yolov8-detection/yolov8/main.py
@@ -2,16 +2,32 @@ import cv2 as cv
 import os
 from ultralytics import YOLO
 
-cap=cv.VIDEOCapture(0)
+cap=cv.VideoCapture(0)
 
 # load model
 model_path=os.path.join("runs", "weights", "best.pt")
 model=YOLO(model_path)
 
+threshold=0.5
+
 while True:
     ret, frame=cap.read()
 
     if ret:
-        pass
+        cv.imshow(frame)
+        results=model(frame)[0]
+
+        for result in results.boxes.data.tolist():
+            x1, y1, x2, y2, score, class_id=result
+
+            if score>threshold:
+                cv.rectangle(frame, (int(x1), int(y1), (int (x2), int(y2)), (0,255,0), 4))
+
+        if cv.waitKey(1)&0xFF==ord('q'):
+            break
+
+cap.release()
+cv.destroyAllWindows()
+
 
 
