891adf2de1439b30de0aabaee51cd4db639d7bdf
Daniel W Liu <117604131+LTKers@users.noreply.github.com>
2025-07-24 10:56:21 -0400

    feat(dataset): add unlabeled validation data

---
+++ YOLOV8/main.py
@@ -0,0 +1,2 @@
+import cv2 as cv
+from ultralytics import YOLO
--- YOLOV8/train.py
+++ YOLOV8/train.py
@@ -1,7 +1,9 @@
+import os
 from ultralytics import YOLO
 
 # load model
 model = YOLO("yolov8n.pt")
 
 # train model
-results = model.train(data="config.yaml", epochs=1)
+config = os.path.join("YOLOV8", "config.yaml")
+results = model.train(data=config, epochs=1)
