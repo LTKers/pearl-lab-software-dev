579058f30266fbadfb3a01a918b2a52b71cd7dd5
Daniel W Liu <117604131+LTKers@users.noreply.github.com>
2025-07-28 00:59:36 -0400

    feat(AprilTag): Add scripts for detection of AprilTags and main to run code.

---
+++ fiducial_markers/apriltag/apriltag_detector.py
@@ -0,0 +1,20 @@
+"""
+AprilTag's and their respective blocks
+The AprilTags come from the family tag16h5.
+The list of AprilTags can be found here https://github.com/AprilRobotics/apriltag-imgs
+
+ID  BLOCK
+1   Blue_1
+2   Blue_2
+3   Green_1
+4   Green_2
+5   Red_1
+6   Red_2
+7   Yellow_1
+8   Yellow_2
+
+"""
+
+import cv2 as cv
+import pupil_apriltags as pat
+
