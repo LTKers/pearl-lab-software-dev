5513e19c421e451e072280f0f52bc248c1335ac5
Daniel W Liu <117604131+LTKers@users.noreply.github.com>
2025-07-27 20:32:07 -0400

    feat(QR): Add preprocessing before QR code detecgtion.

--- qr-code/main.py
+++ qr-code/main.py
@@ -1,4 +1,5 @@
 import cv2 as cv
+import numpy as np
 from qr_code_block_detector import detect_qr
 
 # Open webcam
@@ -10,12 +11,22 @@ while True:
         print("Failed to grab frame.")
         break
 
-    detections = detect_qr(frame)
 
+    gray_img=cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+    blur = cv.GaussianBlur(gray_img, (3, 3), 0)
+    kernel = np.array([[0, -1, 0],
+                   [-1, 5,-1],
+                   [0, -1, 0]])
+
+    sharpened = cv.filter2D(blur, -1, kernel)
+    ret, bw_im = cv.threshold(sharpened, 0, 255, cv.THRESH_BINARY + cv.THRESH_OTSU)
+    detections = detect_qr(bw_im)
+
+    
     for detection in detections:
         text_colour=detection["text_colour"]
         pts = detection["points"]
-        bgr_color = detection["bgr_color"]
+        bgr_color = detection["bgr_colour"]
 
         cv.polylines(frame, [pts], True, bgr_color, 2)
         pt = tuple(pts[0])
@@ -23,6 +34,7 @@ while True:
 
     cv.imshow("QR Detection", frame)
 
+    cv.imshow("bw", bw_im)
     if cv.waitKey(1) & 0xFF == ord('q'):
         break
 
