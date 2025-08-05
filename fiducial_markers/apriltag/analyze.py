15564706f032e2296d18ef98183cbd31912828cc
Daniel W Liu <117604131+LTKers@users.noreply.github.com>
2025-08-05 14:14:31 -0400

    fix(apriltag): fix the video cropping; refactor crop function from main_window to analyze

--- fiducial_markers/apriltag/analyze.py
+++ fiducial_markers/apriltag/analyze.py
@@ -3,9 +3,12 @@ import numpy as np
 import time
 from PyQt5.QtCore import QObject, pyqtSignal
 from apriltag_detector import detect_apriltag
+import math
+import numpy as np
 
 class analyze_apriltag(QObject):
-    feed_frame = pyqtSignal(object)
+    dimension_signal = pyqtSignal(object)
+    feed_frame = pyqtSignal(object, list)
 
     def __init__(self):
         super().__init__()
@@ -13,13 +16,13 @@ class analyze_apriltag(QObject):
         self.previous_time = time.time()
         self.run_program = True
         self.inital_coords = [0, 0]
-
-
+        self.updated_img = np.zeros((100, 100, 3), np.uint8)
 
     def run(self):
         while self.run_program:
-            ret, frame = self.cap.read()
-            height, width = frame.shape[:2]
+            ret, self.frame = self.cap.read()
+            height, width = self.frame.shape[:2]
+            self.tower_order=[]
 
             if not ret:
                 print("Failed to grab frame.")
@@ -29,10 +32,10 @@ class analyze_apriltag(QObject):
             current_time = time.time()
             fps = 1 / (current_time - self.previous_time)
             self.previous_time= current_time
-            cv.putText(frame, f"FPS: {fps:.2f}", [self.inital_coords[0] + 10, self.inital_coords[1] + 30], cv.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+            cv.putText(self.updated_img, f"FPS: {fps:.2f}", [self.inital_coords[0] + 10, self.inital_coords[1] + 30], cv.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
 
             #Gray Scale
-            gray_img=cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+            gray_img=cv.cvtColor(self.updated_img, cv.COLOR_BGR2GRAY)
             detections = detect_apriltag(gray_img)
 
             id_list = []
@@ -76,23 +79,43 @@ class analyze_apriltag(QObject):
 
             if tallest_coords is not None and tallest_coords in tower_dict:
                 for bgr_colour, text_colour, y, pts in tower_dict[tallest_coords]:
-                    cv.polylines(frame, [pts], True, bgr_colour, 2)
-                    cv.putText(frame, text_colour, (pts[0][0], pts[0][1] + 30), cv.FONT_HERSHEY_SIMPLEX, 0.7, bgr_colour, 2)
+                    cv.polylines(self.updated_img, [pts], True, bgr_colour, 2)
+                    cv.putText(self.updated_img, text_colour, (pts[0][0], pts[0][1] + 30), cv.FONT_HERSHEY_SIMPLEX, 0.7, bgr_colour, 2)
+                    self.tower_order.append(text_colour)
 
-                cv.putText(frame, f"Tallest Tower: {tallest_height} blocks", [self.inital_coords[0] + 10, self.inital_coords[1] + 70 ], cv.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 255), 2)
+               
+                cv.putText(self.updated_img, f"Tallest Tower: {tallest_height} blocks", [self.inital_coords[0] + 10, self.inital_coords[1] + 70 ], cv.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 255), 2)
                 
             # cv.imshow("QR Detection" , frame)
-            RGB_frame = cv.cvtColor(frame, cv.COLOR_BGR2RGB)
-            self.feed_frame.emit(RGB_frame)
+            RGB_frame = cv.cvtColor(self.updated_img, cv.COLOR_BGR2RGB)
+            self.feed_frame.emit(RGB_frame, self.tower_order)
 
+            self.dimension_signal.emit("hi")
             if cv.waitKey(1) & 0xFF == ord('q'):
                 break
 
         self.cap.release()
         cv.destroyAllWindows()
-    
-    def update_initial_dimensions(self, new_coords):
-        self.inital_coords = [int(new_coords[0]), int(new_coords[1])]
+
+    def center_crop_resize(self, target_dimensions):
+        h, w, _ = self.frame.shape
+        target_width=target_dimensions[0]
+        target_height=target_dimensions[1]
+        scale = max(target_width / w, target_height / h)
+        new_w, new_h = math.ceil(w * scale), math.ceil(h * scale)
+        resized = cv.resize(self.frame, (new_w, new_h), interpolation = cv.INTER_AREA)
+
+        x_start = (new_w - target_width) // 2
+        y_start = (new_h - target_height) // 2
+
+        cropped = resized[y_start : y_start + target_height, x_start : x_start + target_width]
+
+        orig_x_start = x_start / scale
+        orig_y_start = y_start / scale
+        orig_x_end = (x_start + target_width) / scale
+        orig_y_end = (y_start + target_height) / scale
+
+        self.updated_img = cropped
 
     def stop(self):
         self.run_program = False
--- fiducial_markers/apriltag/main_window.py
+++ fiducial_markers/apriltag/main_window.py
@@ -1,4 +1,4 @@
-from PyQt5.QtWidgets import QApplication, QMainWindow, QDesktopWidget, QLabel, QSizePolicy, QWidget, QHBoxLayout
+from PyQt5.QtWidgets import QApplication, QMainWindow, QDesktopWidget, QLabel, QSizePolicy, QWidget, QHBoxLayout, QVBoxLayout
 from PyQt5.QtGui import QPixmap, QImage, QFontDatabase, QFont, QIcon
 from PyQt5.QtCore import Qt, QObject, pyqtSignal
 
@@ -6,13 +6,13 @@ import cv2 as cv
 import math
 
 class MainWindow(QMainWindow, QObject):
-    dimension_signal = pyqtSignal(list)
+    return_dimension = pyqtSignal(list)
 
     def __init__(self, analyze):
         super().__init__()
         self.setWindowTitle("AprilTag Demo")
         self.initUI()
-
+        self.tower_list=[]
         self.analyze = analyze
 
         screen = QDesktopWidget().screenGeometry()
@@ -24,54 +24,43 @@ class MainWindow(QMainWindow, QObject):
         pos_y = (screen_height - window_height) // 2
         self.setGeometry(pos_x, pos_y, window_width, window_height)
 
-        self.analyze.feed_frame.connect(self.update_image)
+        self.analyze.dimension_signal.connect(self.get_dimensions)
+        self.analyze.feed_frame.connect(self.update_img)
 
     def initUI(self):
-
         self.video_feed = QLabel(self)
         self.video_feed.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
         self.video_feed.setMinimumSize(320, 240)  
 
         central_widget = QWidget()
         self.setCentralWidget(central_widget)   
-        self.main_area_layout = QHBoxLayout()
-        self.main_area_layout.addWidget(self.video_feed)
-        self.main_area_layout.addWidget(self.video_feed)
+        main_area_layout = QHBoxLayout()
+        main_area_layout.addWidget(self.video_feed)
+        main_area_layout.addWidget(self.video_feed)
+        central_widget.setLayout(main_area_layout)
 
-        central_widget.setLayout(self.main_area_layout)
+        block_stack = QWidget()
+        block_layout = QVBoxLayout()
+        block_stack.setLayout(block_layout)
 
-    def update_image(self, cv_img):
+        for i in range(4):
+            block_1 = QLabel(self)
+            block_pixmap_1 = QPixmap()
+            block_1.setPixmap(block_pixmap_1)
+
+
+    def get_dimensions(self, obj):
         target_width = self.video_feed.width()
         target_height = self.video_feed.height()
+        self.return_dimension.emit([target_width, target_height])
 
-        img = self.center_crop_resize(cv_img, target_width, target_height)
-
-        h, w, ch = img.shape
-
+    def update_img(self, cv_img, tower_list):
+        h, w, ch = cv_img.shape
         bytes_per_line = ch * w
-        camera_image = QImage(img.tobytes(), w, h, bytes_per_line, QImage.Format_RGB888)
+        self.tower_list = tower_list
+        camera_image = QImage(cv_img.tobytes(), w, h, bytes_per_line, QImage.Format_RGB888)
         self.video_feed.setPixmap(QPixmap.fromImage(camera_image))
-
-
-    def center_crop_resize(self, frame, target_width, target_height):
-        h, w, _ = frame.shape
-
-        scale = max(target_width / w, target_height / h)
-        new_w, new_h = math.ceil(w * scale), math.ceil(h * scale)
-        resized = cv.resize(frame, (new_w, new_h), interpolation = cv.INTER_AREA)
-
-        x_start = (new_w - target_width) // 2
-        y_start = (new_h - target_height) // 2
-
-        cropped = resized[y_start : y_start + target_height, x_start : x_start + target_width]
-
-        orig_x_start = x_start / scale
-        orig_y_start = y_start / scale
-
-        self.dimension_signal.emit([orig_x_start, orig_y_start])
-
-        return cropped
-
+    
     def closeEvent(self, event):
         self.analyze.stop()
         return super().closeEvent(event)
